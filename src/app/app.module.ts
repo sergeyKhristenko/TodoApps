6a99e1a96177ee7d6f7bb4699e637f80602a895b
sergeyKhristenko <sergey.khristenko@gmail.com>
2018-06-05 18:38:31 +0300

    login: added template with styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NotesEditorComponent } from './notes-editor/notes-editor.component';
@@ -15,10 +16,42 @@ import { reducers } from './store';
 import * as fromReducers from './store/reducers/notes.reducer';
 import * as fromActions from './store/actions/notes.action';
 import { NotesEffects } from './store/effects/notesEffects';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+  { path: '', component: LoginComponent },
+  {
+    path: 'todos',
+    component: NotesEditorComponent,
+    children: [
+      {
+        path: '',
+        component: NotesListComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
-  declarations: [AppComponent, NotesEditorComponent, ColorPickerComponent, NotesListComponent, NoteItemComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule, StoreModule.forRoot(reducers), EffectsModule.forRoot([NotesEffects])],
+  declarations: [
+    AppComponent,
+    NotesEditorComponent,
+    ColorPickerComponent,
+    NotesListComponent,
+    NoteItemComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot([NotesEffects]),
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
